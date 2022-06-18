b8e6b799d6864ecf994cdec31d91a369e53639a5
Emanuele Moncada <em@zerowk.it>
2022-06-18 16:02:24 +0200

    update garage exercises

--- inheritance/garage/Garage.java
+++ inheritance/garage/Garage.java
@@ -2,22 +2,23 @@ package inheritance.garage;
 
 public class Garage {
 
-    private int maxCar;
-    private int maxBike;
-    private int maxRugged;
+    private int maxCar=0;
+    private int maxBike = 0;
+    private int maxRugged = 0;
 
     private int currentCar = 0;
     private int currentBike = 0;
     private int currentRugged = 0;
-
-    private int max = getMaxVeichle();
-    private Veichle[] postiAuto = new Veichle[max];
 
     Garage(int maxCar, int maxBike, int maxRugged) {
         setMaxCar(maxCar);
         setMaxBike(maxBike);
         setMaxRugged(maxRugged);
     }
+    
+    private int totMax = getMax();
+    private Veichle[] postiParcheggio = new Veichle[totMax];
+    private int index = 0;
 
     public void setMaxBike(int maxBike) {
         this.maxBike = maxBike;
@@ -31,10 +32,49 @@ public class Garage {
         this.maxRugged = maxRugged;
     }
 
-    public int getMaxVeichle() {
-        return this.maxBike + this.maxCar + this.maxRugged;
+    public int getMax() {
+        return maxCar+maxRugged+maxBike;
     }
 
-    
-        
+    public Veichle addVeichle(Car veichle) {
+        if (currentCar > maxCar) {
+            System.out.println("Non ci sono più posti per le auto");
+        } else {
+            currentCar++;
+            postiParcheggio[index] = veichle;
+            index++;
+            System.out.println("Posti rimasti per le auto [" + (maxCar - currentCar) + "]");
+            System.out.println("Ticket parcheggio [" + index + "]");
+        }
+        return veichle;
+    }
+
+    public Veichle addVeichle(Bike veichle) {
+        if (currentBike > maxBike) {
+            System.out.println("Non ci sono più posti per le motociclette");
+        } else {
+            currentBike++;
+            postiParcheggio[index] = veichle;
+            index++;
+            System.out.println("Posti rimasti per le moto ["+(maxBike-currentBike)+"]");
+            System.out.println("Ticket parcheggio ["+index+"]");
+        }
+        return veichle;
+    }
+
+    public Veichle addVeichle(Rugged veichle) {
+        if (currentRugged > maxRugged) {
+            System.out.println("Non ci sono più posti per i furgoni ");
+        } else {
+            currentRugged++;
+            postiParcheggio[index] = veichle;
+            index++;
+            System.out.println("Posti rimasti per i furgoni [" + (maxRugged - currentRugged) + "]");
+            System.out.println("Ticket parcheggio [" + index + "]");
+
+        }
+        return veichle;
+    }
+
 }
+
--- inheritance/garage/Main.java
+++ inheritance/garage/Main.java
@@ -8,6 +8,10 @@ public class Main {
         Veichle moto = new Bike(2012, "Tmax", "Benzina", 2, "Sportiva", 4);
         Veichle cargo = new Rugged(2005, "Fiorino", "Gas", 12, 1200);
 
-        System.out.println("Capienza massima ["+portosalvo.getMaxVeichle()+"]");
+        System.out.println("Capienza massima ["+portosalvo.getMax()+"]");
+        portosalvo.addVeichle(macchina);
+        portosalvo.addVeichle(moto);
+        portosalvo.addVeichle(cargo);
+
     }
 }
