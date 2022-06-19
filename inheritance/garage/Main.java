b18930824644ce9c93c3b77646cc11322bce4160
Emanuele Moncada <em@zerowk.it>
2022-06-19 03:30:47 +0200

    upload exercises

--- inheritance/garage/Main.java
+++ inheritance/garage/Main.java
@@ -2,13 +2,14 @@ package inheritance.garage;
 
 public class Main {
     public static void main(String[] args) {
-        Garage portosalvo = new Garage(10, 20, 4);
+        Garage portosalvo = new Garage(5, 10, 1);
 
         Veichle macchina = new Car(1990, "Audi", "Disel", 8, 5);
         Veichle moto = new Bike(2012, "Tmax", "Benzina", 2, "Sportiva", 4);
         Veichle cargo = new Rugged(2005, "Fiorino", "Gas", 12, 1200);
 
         System.out.println("Capienza massima ["+portosalvo.getMax()+"]");
+
         portosalvo.addVeichle(macchina);
         portosalvo.addVeichle(moto);
         portosalvo.addVeichle(cargo);
