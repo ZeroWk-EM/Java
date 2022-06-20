bdbe1cb31f8b92821f3f367406841162466f0430
Emanuele Moncada <em@zerowk.it>
2022-06-20 09:37:38 +0200

    Add licenzePlate attribute

--- inheritance/garage/Main.java
+++ inheritance/garage/Main.java
@@ -4,12 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Garage portosalvo = new Garage(5, 10, 1);
 
-        Veichle macchina = new Car(1990, "Audi", "Disel", 8, 5);
-        Veichle moto = new Bike(2012, "Tmax", "Benzina", 2, "Sportiva", 4);
-        Veichle cargo = new Rugged(2005, "Fiorino", "Gas", 12, 1200);
+        Veichle macchina = new Car("AAS 345 FG3",1990, "Audi", "Disel", 8, 5);
+        Veichle moto = new Bike("LLO 112 8RJ",2012, "Tmax", "Benzina", 2, "Sportiva", 4);
+        Veichle cargo = new Rugged("DDT 109 KKP",2005, "Fiorino", "Gas", 12, 1200);
 
         System.out.println("Capienza massima ["+portosalvo.getMax()+"]");
         
+        macchina.accendi();
+        moto.accendi();
+        cargo.accendi();
 
     }
 }
