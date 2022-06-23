d718caceccbb43863b7d1321738a53ca92ef80da
Emanuele Moncada <em@zerowk.it>
2022-06-23 14:57:39 +0200

    testing classes

--- videoteca/Main.java
+++ videoteca/Main.java
@@ -6,16 +6,40 @@ import videoteca.prodotti.media.*;
 public class Main {
     public static void main(String[] args) {
 
-        Shop unieuro = new Shop();
+        Shop unieuro = new Shop(8);
 
-        Device iphone13 = new Phone(002, "Apple", "Iphone 13", "SSDIWOQ2245", 2021, "IOS 15", 845.50);
+        System.out.println("Numero massimo di oggetti [" + unieuro.getLen() + "]");
+
+        Device iphone12 = new Phone(129, "Apple", "Iphone 12", "LPDIW304L4", 2019, "IOS 14", 750.00);
+        Device iphone13 = new Phone(248, "Apple", "Iphone 13", "SSDIWOQ224", 2021, "IOS 15", 845.50);
+        Device iphone17 = new Phone(248, "Apple", "Iphone 13", "SSDIWOQ224", 2021, "IOS 15", 845.50);
+
+        Device ipad = new Tablet(924, "Apple", "Ipad 10.2", "SLLMN4MNJ89", 2022, "IPADOS 15", 500.0);
+        Device ipdaPro = new Tablet(324, "Apple", "Ipad PRO", "SSP234MNSJ", 2020, "IPADOS 15", 1000.0);
+
+        Media uncharched = new Game(213, "Uncharted: collector edition", "Adventure", 2017, true, 40);
         Media jbond = new Film(532, "007 Casino Royale", "action", 120, 2006, 13.00);
-        Device iphone12 = new Phone(002, "Apple", "Iphone 12", "SSDIWOQ2245", 2021, "IOS 15", 845.50);
+        Media imdragon = new Music(997, "Beliver", "POP", "Image Dragon", 10, 2016, 10);
 
-        unieuro.addProdotto(iphone13);
-        unieuro.addProdotto(jbond);
         unieuro.addProdotto(iphone12);
+        unieuro.addProdotto(iphone13);
+        unieuro.addProdotto(iphone17);
+        unieuro.addProdotto(ipad);
+        unieuro.addProdotto(ipdaPro);
+
+        unieuro.addProdotto(uncharched);
+        unieuro.addProdotto(jbond);
+        unieuro.addProdotto(imdragon);
 
         unieuro.shopState();
+
+        System.out.println("\nOggetti totali in inventario [" + unieuro.getLen() + "]");
+
+        System.out.println("Numero di telefoni [" + unieuro.phoneStatus() + "]");
+        System.out.println("Numero di tablet [" + unieuro.tabletStatus() + "]");
+        System.out.println("Numero di giochi [" + unieuro.gameStatus() + "]");
+        System.out.println("Numero di film [" + unieuro.filmStatus() + "]");
+        System.out.println("Numero di cd musicali [" + unieuro.musicStatus() + "]");
+
     }
 }
