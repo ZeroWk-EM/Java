f000eeeef56348833aca231970256d8ecac6cc12
Emanuele Moncada <em@zerowk.it>
2022-07-07 12:09:44 +0200

    add exercises

---
+++ market/Main.java
@@ -0,0 +1,20 @@
+package market;
+
+public class Main {
+    public static void main(String[] args) {
+        Catalog<Furniture> catalogFurniture = new Catalog<Furniture>();
+
+        Furniture desk = new Furniture("Scrivania Ikea", 99.5, 6.8, 9.9, 10.00);
+        Furniture chair = new Furniture("Lilla Chair", 350.00, 4.6, 22.5, 1.5);
+        Furniture lamp = new Furniture("Lava Lamp", 650.00, 4.1, 72.5, 10.5);
+
+        catalogFurniture.addArticle(desk);
+        catalogFurniture.addArticle(chair);
+        catalogFurniture.addArticle(lamp);
+
+        System.out.println(catalogFurniture);
+
+        System.out.println(catalogFurniture.getArticleCatalogUnderPrice(100.00));
+    }
+
+}
